1faff7632b85db090640833fd3e48d79d3450c37
Ro0z <roz.unlp@gmail.com>
2022-08-17 10:34:02 -0300

    1008

--- ejercicio 10.py
+++ ejercicio 10.py
@@ -1,21 +1,21 @@
 print("")
 print("Ejercicio: NUMEROS PRIMOS ENTRE 1 Y 1000.") 
 print("")
-primero = 2 
+primero=2
 limite = 1000 
 cont=0
 for i in range(primero, limite): 
     primo = True
     j=2
 
-    while (i > j) and (primo == True):
+    while (i>j) and (primo==True):
         if i%j == 0:
             primo = False
             break 
         else: j=j+1
     if primo == True: 
         print(i, "es primo.")
-        Cont = Cont + 1
+        Cont=Cont+1
 print("\nSALIDA: ")
 print ("") 
 print("Entre", primero, "y" , limite, "hay", Cont, "n primos")
